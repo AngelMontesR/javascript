bfce426b61cefd38f9423c90d2aab936d16d3f80
Luis Angel Montes Reyes <lmontes@hdlatinoamerica.com>
2023-06-08 14:02:46 -0600

    Modificación  Se agrega campo texto y muestra valor

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,7 @@
-alert('Hola Mundo')
+/*
+    ===== Código de JavaScript =====
+    Campo de texto, tomar el valor y mostrarlo en pantalla
+*/
+
+const nombre = prompt('¿Cuál es tu nombre?');
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`;
