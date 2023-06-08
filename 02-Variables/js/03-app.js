b91e8390c3ae3ea3f0eb63f88c5ae98693f8786d
Luis Angel Montes Reyes <lmontes@hdlatinoamerica.com>
2023-06-08 17:36:38 -0600

    Creación  Se crea js de variables con Const y buenas practicas

---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,12 @@
+/*
+    ===== Variables Const =====
+
+    Diferencias entre const y let
+    - La variable const no se puede inicializar sin un valor.
+    - La variable const no se puede reasignar.
+
+*/
+
+const producto = "Monitor de 24 pulgadas";
+
+console.log(producto);
