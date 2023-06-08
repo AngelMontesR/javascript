90b80d4442b55f7190c0ef232f8b38019f23b33d
Luis Angel Montes Reyes <lmontes@hdlatinoamerica.com>
2023-06-08 17:32:24 -0600

    Creación  Se crea js de variables con let y buenas practicas

---
+++ 02-Variables/js/02-app.js
@@ -0,0 +1,14 @@
+/*
+    ===== Variable let =====
+    La variable let es la forma moderna de declarar variables, se utiliza para variables que van a cambiar su valor.
+    La variable let se puede declarar de dos formas:
+    - Declaración: Se declara la variable y se le asigna un valor.
+    - Inicialización: Se declara la variable y se le asigna un valor.
+
+    La variable let se puede declarar de dos formas:
+    - CamelCase: Se escribe la primera palabra en minúscula y las siguientes palabras en mayúscula.
+    - SnakeCase: Se escribe la primera palabra en minúscula y las siguientes palabras separadas por un guión bajo.
+
+*/
+
+let producto = "Monitor de 24 pulgadas"; 
