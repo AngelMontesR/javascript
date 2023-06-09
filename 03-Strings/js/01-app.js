5f3564a6c7f216929424e173aedbe0c8c580a169
Luis Angel Montes Reyes <lmontes@hdlatinoamerica.com>
2023-06-09 09:42:59 -0600

    Creación  Se crea js de variables String  y buenas practicas

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,13 @@
+/*
+    ===== String  =====
+    
+
+*/
+
+const producto = 'Monitor 20 Pulgadas'; // String con comillas simples (más común)
+const producto2 = String('Monitor 24 Pulgadas'); // String con constructor (no es muy común)
+const producto3 = new String('Monitor 27 Pulgadas'); // String con constructor (no es muy común)
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
